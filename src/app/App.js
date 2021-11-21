f5ce2a2a9d755e92e231a61ee3756d030e53d934
joserhernandez <00366718@uca.edu.sv>
2021-11-20 21:03:30 -0600

    add fetch data

--- src/app/App.js
+++ src/app/App.js
@@ -6,15 +6,47 @@ class App extends Component {
         super();
         this.state = {
             name: '',
-            price: ''
+            price: '',
+            products: []
         };
         this.addProduct = this.addProduct.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
     addProduct(e) {
-        console.log(this.state);
+        //console.log(this.state);
+        fetch('/api/products', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            M.toast({html: 'Product Saved'});
+            this.setState({name: '', price: ''});
+            this.fetchProducts();
+        })
+        .catch(err => console.error(err));
+
         e.preventDefault();
+    }
+
+    componentDidMount(){
+        this.fetchProducts();
+    }
+
+    fetchProducts() {
+        fetch('/api/products')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.setState({products: data});
+                this.console.log(this.state.products);
+            });
     }
 
     handleChange(e) {
@@ -41,12 +73,12 @@ class App extends Component {
                                     <form onSubmit={this.addProduct}>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input name="name" onChange={this.handleChange} type="text" placeholder="Product Name"/>
+                                                <input name="name" onChange={this.handleChange} type="text" placeholder="Product Name" value={this.state.name}/>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input name="price" onChange={this.handleChange} type="number" min="0.00" step="0.01" placeholder="Product Price ($)"/>
+                                                <input name="price" onChange={this.handleChange} type="number" min="0.00" step="0.01" placeholder="Product Price ($)" value={this.state.price}/>
                                             </div>
                                         </div>
                                         <button type="submit" className="btn red darken-4">Add</button>
@@ -55,7 +87,26 @@ class App extends Component {
                             </div>
                         </div>
                         <div className="col s7">
-                            
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Price $</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.products.map(product => {
+                                            return (
+                                                <tr key={product._id}>
+                                                    <td>{product.name}</td>
+                                                    <td>{product.price}</td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
